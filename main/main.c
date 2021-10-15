b99085ada3c88ec4c1c0b264f9fe36739ca0856f
Sebastian Ohl <sebastian@ohl.de>
2021-10-15 13:48:15 +0200

    reading PWM working

--- main/main.c
+++ main/main.c
@@ -1,135 +1,61 @@
-/* Uart Events Example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
-#include "driver/uart.h"
 #include "freertos/queue.h"
-#include "esp_log.h"
-#include "sdkconfig.h"
 #include "driver/gpio.h"
 
-/**
- * This is a example which echos any data it receives on UART back to the sender using RS485 interface in half duplex mode.
-*/
-#define TAG "RS485_ECHO_APP"
+#define ESP_INTR_FLAG_DEFAULT 0
 
-// Note: Some pins on target chip cannot be assigned for UART communication.
-// Please refer to documentation for selected board and target to configure pins using Kconfig.
-#define ECHO_TEST_TXD   (CONFIG_ECHO_UART_TXD)
-#define ECHO_TEST_RXD   (CONFIG_ECHO_UART_RXD)
+static xQueueHandle gpio_evt_queue = NULL;
 
-// RTS for RS485 Half-Duplex Mode manages DE/~RE
-#define ECHO_TEST_RTS   (CONFIG_ECHO_UART_RTS)
-
-// CTS is not used in RS485 Half-Duplex Mode
-#define ECHO_TEST_CTS   (UART_PIN_NO_CHANGE)
-
-#define BUF_SIZE        (127)
-#define BAUD_RATE       (CONFIG_ECHO_UART_BAUD_RATE)
-
-// Read packet timeout
-#define PACKET_READ_TICS        (1000 / portTICK_RATE_MS)
-#define ECHO_TASK_STACK_SIZE    (2048)
-#define ECHO_TASK_PRIO          (10)
-#define ECHO_UART_PORT          (CONFIG_ECHO_UART_PORT_NUM)
-
-// Timeout threshold for UART = number of symbols (~10 tics) with unchanged state on receive pin
-#define ECHO_READ_TOUT          (3) // 3.5T * 8 = 28 ticks, TOUT=3 -> ~24..33 ticks
-
-static void echo_send(const int port, const char* str, uint8_t length)
+static uint32_t val_begin_of_sample = 0;
+static uint32_t val_end_of_sample = 0;
+static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
-    if (uart_write_bytes(port, str, length) != length) {
-        ESP_LOGE(TAG, "Send data critical failure.");
-        // add your code to handle sending failure here
-        abort();
+    if (gpio_get_level(CONFIG_SERVO_INPUT_PORT_NUM)) {
+        val_begin_of_sample = esp_timer_get_time();
+        val_end_of_sample = val_begin_of_sample;
+    } else {
+        val_end_of_sample = esp_timer_get_time();
+        uint32_t pulse_with = val_end_of_sample - val_begin_of_sample;
+        xQueueSendFromISR(gpio_evt_queue, &pulse_with, NULL);
     }
-}
-
-// An example of echo test with hardware flow control on UART
-static void echo_task(void *arg)
-{
-    const int uart_txnum = ECHO_UART_PORT;
-    const int uart_rxnum = 0;
-    uart_config_t uart_config = {
-        .baud_rate = BAUD_RATE,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .rx_flow_ctrl_thresh = 122,
-        .source_clk = UART_SCLK_APB,
-    };
-
-    // Set UART log level
-    esp_log_level_set(TAG, ESP_LOG_INFO);
-
-    ESP_LOGI(TAG, "Start RS485 application test and configure UART.");
-
-    gpio_pad_select_gpio(ECHO_TEST_RTS);
-    gpio_set_direction(ECHO_TEST_RTS, GPIO_MODE_OUTPUT);
-    gpio_set_level(ECHO_TEST_RTS, 0);
-
-    ESP_ERROR_CHECK(uart_driver_install(uart_txnum, BUF_SIZE * 2, 0, 0, NULL, 0));
-    ESP_ERROR_CHECK(uart_param_config(uart_txnum, &uart_config));
-    ESP_ERROR_CHECK(uart_set_pin(uart_txnum, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
-    ESP_ERROR_CHECK(uart_set_mode(uart_txnum, UART_MODE_RS485_HALF_DUPLEX));
-    ESP_ERROR_CHECK(uart_set_rx_timeout(uart_txnum, ECHO_READ_TOUT));
-
-    ESP_ERROR_CHECK(uart_driver_install(uart_rxnum, BUF_SIZE * 2, 0, 0, NULL, 0));
-    ESP_ERROR_CHECK(uart_param_config(uart_rxnum, &uart_config));
-
-    // Allocate buffers for UART
-    uint8_t* data = (uint8_t*) malloc(BUF_SIZE);
-
-    ESP_LOGI(TAG, "UART start recieve loop.\r\n");
-    echo_send(uart_txnum, "Start RS485 UART test.\r\n", 24);
-
-    while(1) {
-        //Read data from UART
-        gpio_set_level(2, 0);
-        int len = uart_read_bytes(uart_rxnum, data, BUF_SIZE, PACKET_READ_TICS);
-        gpio_set_level(2, 1);
-
-        //Write data back to UART
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        if (len > 0) {
-            echo_send(uart_txnum, "\r\n", 2);
-            char prefix[] = "RS485 Received: [";
-            echo_send(uart_txnum, prefix, (sizeof(prefix) - 1));
-            ESP_LOGI(TAG, "Received %u bytes:", len);
-            printf("[ ");
-            for (int i = 0; i < len; i++) {
-                printf("0x%.2X ", (uint8_t)data[i]);
-                echo_send(uart_txnum, (const char*)&data[i], 1);
-                // Add a Newline character if you get a return charater from paste (Paste tests multibyte receipt/buffer)
-                if (data[i] == '\r') {
-                    echo_send(uart_txnum, "\n", 1);
-                }
-            }
-            printf("] \n");
-            echo_send(uart_txnum, "]\r\n", 3);
-        } else {
-            // Echo a "." to show we are alive while we wait for input
-            echo_send(uart_txnum, ".", 1);
-            ESP_ERROR_CHECK(uart_wait_tx_done(uart_txnum, 10));
-        }
-    }
-    vTaskDelete(NULL);
 }
 
 void app_main(void)
 {
-    //A uart read/write example without event queue;
-    xTaskCreate(echo_task, "uart_echo_task", ECHO_TASK_STACK_SIZE, NULL, ECHO_TASK_PRIO, NULL);
+    //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    io_conf.pin_bit_mask = (1ULL<<CONFIG_SERVO_INPUT_PORT_NUM);
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(100, sizeof(uint32_t));
+
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(CONFIG_SERVO_INPUT_PORT_NUM, gpio_isr_handler, (void*) CONFIG_SERVO_INPUT_PORT_NUM);
+
+    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+
+    while(1) {
+        uint32_t pulse_width;
+        if(xQueueReceive(gpio_evt_queue, &pulse_width, 500/portTICK_RATE_MS)) {
+            printf("val: %d us\n", pulse_width);
+        } else {
+            printf("timeout\n");
+        }
+    }
 }
