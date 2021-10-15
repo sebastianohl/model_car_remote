f714f796b2b9e87db5b7d770af6159c476ba18fe
Sebastian Ohl <sebastian@ohl.de>
2021-10-15 14:32:32 +0200

    working output pwm

--- main/main.c
+++ main/main.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 
 #define ESP_INTR_FLAG_DEFAULT 0
 
@@ -23,6 +25,22 @@ static void IRAM_ATTR gpio_isr_handler(void* arg)
         uint32_t pulse_with = val_end_of_sample - val_begin_of_sample;
         xQueueSendFromISR(gpio_evt_queue, &pulse_with, NULL);
     }
+}
+
+uint32_t DutyCyclePercentageToDuty(float per)
+{
+    return per/100.0f * pow(2,LEDC_TIMER_13_BIT);
+
+}
+
+float DutyCycleScale(float per, float scale)
+{
+    return 7.5+((7.5f-per)*scale);
+}
+
+float DutyCycleUsToPercentage(uint32_t us)
+{
+    return us / 200.0f /* us to percent at 50hz*/;
 }
 
 void app_main(void)
@@ -48,12 +66,41 @@ void app_main(void)
     //hook isr handler for specific gpio pin
     gpio_isr_handler_add(CONFIG_SERVO_INPUT_PORT_NUM, gpio_isr_handler, (void*) CONFIG_SERVO_INPUT_PORT_NUM);
 
+    /*
+     * Prepare and set configuration of timers
+     * that will be used by LED Controller
+     */
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_13_BIT, // resolution of PWM duty
+        .freq_hz = 50,                      // frequency of PWM signal
+        .speed_mode = LEDC_LOW_SPEED_MODE,           // timer mode
+        .timer_num = LEDC_TIMER_1,            // timer index
+        .clk_cfg = LEDC_AUTO_CLK,              // Auto select the source clock
+    };
+    // Set configuration of timer0 for high speed channels
+    ledc_timer_config(&ledc_timer);
+
+    ledc_channel_config_t ledc_channel = {
+            .channel    = LEDC_CHANNEL_0,
+            .duty       = 0,
+            .gpio_num   = CONFIG_SERVO_OUTPUT_PORT_NUM,
+            .speed_mode = LEDC_LOW_SPEED_MODE,
+            .hpoint     = 0,
+            .timer_sel  = LEDC_TIMER_1
+    };
+    ledc_channel_config(&ledc_channel);
+    ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, DutyCyclePercentageToDuty(7.5f));
+    ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
+
     printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
 
     while(1) {
         uint32_t pulse_width;
         if(xQueueReceive(gpio_evt_queue, &pulse_width, 500/portTICK_RATE_MS)) {
-            printf("val: %d us\n", pulse_width);
+            printf("val: %d us %f %%\n", pulse_width, DutyCycleUsToPercentage(pulse_width));
+            ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, 
+                DutyCyclePercentageToDuty(DutyCycleScale(DutyCycleUsToPercentage(pulse_width), 1.0f)));
+            ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
         } else {
             printf("timeout\n");
         }
